a1ba460c8becef720371ecc6b5b16f5cb5216db1
Антон Швец <a.shvec@santehnika-tut.ru>
2022-09-13 11:29:50 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import "@fontsource/roboto";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import AuthProvider from "./providers/AuthProvider";
 
 ReactDOM.render(
   <React.Fragment>
-    <AuthProvider>
-      <Router>
+
         <App />
-      </Router>
-    </AuthProvider>
+
+
   </React.Fragment>,
   document.getElementById("root")
 );
