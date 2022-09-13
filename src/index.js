f11715b0e241e6c7ccd6779fe58ede43d8dc034e
Антон Швец <a.shvec@santehnika-tut.ru>
2022-09-13 11:42:34 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "@fontsource/roboto";
-import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
-import AuthProvider from "./providers/AuthProvider/index";
+ import React from "react";
+ import ReactDOM from "react-dom";
+// import "@fontsource/roboto";
+// import "./index.css";
+// import { BrowserRouter as Router } from "react-router-dom";
+ import App from "./App";
+// import AuthProvider from "./providers/AuthProvider/index";
 
-ReactDOM.render(
-  <React.Fragment>
-    <AuthProvider>
-      <Router>
-        <App />
-      </Router>
-    </AuthProvider>
-  </React.Fragment>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
